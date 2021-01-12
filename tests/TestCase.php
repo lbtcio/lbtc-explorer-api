51c996954ba5dd83185f88051b9c9aaa211f867d
WuDing <44403890+KingWuDing@users.noreply.github.com>
2021-01-12 19:09:19 +0800

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,11 @@
 class TestCase extends Laravel\Lumen\Testing\TestCase
 {
     /**
+    
      * Creates the application.
      *
      * @return \Laravel\Lumen\Application
+     
      */
     public function createApplication()
     {
